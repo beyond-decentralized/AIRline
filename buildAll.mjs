40f4b94591a2250443bca81f88bca29e7df7af92
Artem Shamsutdinov <Artem.V.Shamsutdinov@dataindependence.net>
2022-09-07 16:04:24 -0600

    Added build all script.

---
+++ buildAll.mjs
@@ -0,0 +1,117 @@
+import { spawn } from 'child_process';
+
+const projectDirectoriesInBuildOrder = [
+    'apps/topics',
+    'apps/conversations',
+    'apps/tasks'
+]
+
+const reactUiProjects = [
+    'components',
+    'conversations',
+    'tasks'
+]
+
+try {
+    await buildPeerFramework('AIRline', projectDirectoriesInBuildOrder, true)
+    await buildUI('react', reactUiProjects)
+} catch (e) {
+    console.log(e)
+}
+
+async function buildPeerFramework(
+    frameworkDirectoryName,
+    projectDirectoriesInBuildOrder,
+    runRushUpdate
+) {
+    process.chdir('../' + frameworkDirectoryName);
+    if (runRushUpdate) {
+        await wireInDependencies(frameworkDirectoryName)
+    }
+    await buildProjects(projectDirectoriesInBuildOrder, 'rollup', ['-c']);
+}
+
+async function buildUI(
+    uiType,
+    uiProjects
+) {
+    process.chdir('UIs/' + uiType);
+    await buildProjects(uiProjects, 'npm', ['run', 'build']);
+    process.chdir('../..');
+}
+
+async function buildProjects(
+    projectsDirectoriesInBuildOrder,
+    command,
+    parameters
+) {
+    for (const projectDirectory of projectsDirectoriesInBuildOrder) {
+        process.stdout.write(`
+        RUNNING 'rollup -c' in ${projectDirectory}
+
+        `)
+        const directoryDepth = projectDirectory.split('/');
+        let navigateBackPath = '..'
+        for (let i = 1; i < directoryDepth.length; i++) {
+            navigateBackPath = '../' + navigateBackPath
+        }
+        process.chdir('./' + projectDirectory);
+
+        const returnCode = await execute(projectDirectory, command, parameters);
+
+        process.chdir(navigateBackPath);
+
+        if (returnCode != 0) {
+            throw new Error(`
+        Suspending after ${projectDirectory}
+        `)
+        }
+    };
+}
+
+async function wireInDependencies(
+    frameworkName
+) {
+    await execute(frameworkName, 'rush', ['update'])
+}
+
+async function execute(
+    operation,
+    command,
+    parameters
+) {
+    return new Promise((resolve, _reject) => {
+        if (/^win/.test(process.platform)) {
+            parameters = [
+                '/s',
+                '/c',
+                command,
+                ...parameters
+            ]
+            command = 'cmd'
+        }
+
+        const runCommand = spawn(command, parameters);
+
+        runCommand.stdout.on("data", data => {
+            console.log(`${data}`)
+        });
+
+        runCommand.stderr.on("data", data => {
+            console.log(`${data}`)
+        });
+
+        runCommand.on('error', (error) => {
+            console.log(`${error.message}`);
+        });
+
+        runCommand.on("close", code => {
+            console.log(`
+        ${code ? 'ERROR' : 'DONE'}: ${operation}
+
+    `);
+            resolve(code)
+        });
+    })
+
+}
