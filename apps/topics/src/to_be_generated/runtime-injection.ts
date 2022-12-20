d9fb66dd6ff8064eff51c58eeeb2753e9dd2da55
Artem Shamsutdinov <Artem.V.Shamsutdinov@dataindependence.net>
2022-12-20 14:33:14 -0700

    dependecy injection conversion

--- apps/topics/src/to_be_generated/runtime-injection.ts
+++ apps/topics/src/to_be_generated/runtime-injection.ts
@@ -1,27 +1,23 @@
-import { TOPIC_API, topics, THEME_API } from './app-declaration'
+import { application } from './app-declaration'
+import { app } from '@airport/direction-indicator'
 import { TopicDao, ThemeDao } from '../dao/dao'
 import { ThemeApi } from '../api/ThemeApi'
 import { TopicApi } from '../api/TopicApi'
-import { APPLICATION_LOADER } from '@airport/web-tower'
+import { setApplicationLoader } from '@airport/web-tower'
 import { ApplicationLoader } from './ApplicationLoader'
 
-export const THEME_DAO = topics.token<ThemeDao>({
-    class: ThemeDao,
-    interface: 'ThemeDao',
-    token: 'THEME_DAO'
+const airApp = app(application)
+
+airApp.register(
+    ThemeApi, TopicApi, ThemeDao,
+    TopicDao
+)
+
+airApp.setDependencies(ThemeApi, {
+    themeDao: ThemeDao
 })
-export const TOPIC_DAO = topics.token<TopicDao>({
-    class: TopicDao,
-    interface: 'TopicDao',
-    token: 'TOPIC_DAO'
+airApp.setDependencies(TopicApi, {
+    topicDao: TopicDao
 })
 
-THEME_API.setClass(ThemeApi)
-THEME_API.setDependencies({
-    themeDao: THEME_DAO
-})
-TOPIC_API.setClass(TopicApi)
-TOPIC_API.setDependencies({
-    topicDao: TOPIC_DAO
-})
-APPLICATION_LOADER.setClass(ApplicationLoader)
+setApplicationLoader(ApplicationLoader)
